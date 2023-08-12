4e650519297b56c4e22f255f3ee70d14674f4972
Matheus Spindula <matheussopluiz@outlook.com>
2023-08-12 19:50:19 -0300

    Total cost and detail xmls

--- faturamento.py
+++ faturamento.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import math
 import locale
+import os
 
 from openpyxl import Workbook, load_workbook
 from openpyxl.worksheet.worksheet import Worksheet
 from typing import List
 from datetime import datetime
-from openpyxl.styles import Font, PatternFill, NamedStyle, Alignment
+from openpyxl.styles import Font, PatternFill, NamedStyle, Alignment, Border
+from openpyxl.styles import Side
 from unidecode import unidecode
 
 dictionary_exams = [
@@ -31,16 +33,19 @@ class Employee:
     exams: str
     function: str
     data: str
+    examsCost: List
     typeExam: str
 
-    def __init__(self, name: str = '', cost: float = 0.0, function: str = '',
-                 exams: str = '', data: str = '', typeExam: str = '',):
+    def __init__(self, examsCost: List = [], name: str = '', cost: float = 0.0,
+                 function: str = '', exams: str = '', data: str = '',
+                 typeExam: str = '',):
         self.name = name
         self.cost = cost
         self.function = function
         self.exams = exams
         self.data = data
         self.typeExam = typeExam
+        self.examsCost = examsCost
 
 
 class Company:
@@ -120,6 +125,7 @@ class Billing:
 # WORKBOOK_PATH_FUNC = ROOT_FOLDER / 'examesRealizados.xlsx'
 WORKBOOK_PATH_COMPANY = './ValoresEmpresas.xlsx'
 WORKBOOK_PATH_FUNC = './examesRealizados.xlsx'
+hasDetailed = False
 
 
 def getExams(company_name, list_of_company, newCompany):
@@ -149,29 +155,61 @@ def setDateXmlsToString(dataInXmls) -> str:
 
 def styleXmls(wb):
     # Estilo da tabela
+    bd = Side(style='thin', color="000000")
     style_header = NamedStyle(name="style_header")
     style_header.font = Font(size=12, color='FFFFFF', name='Arial', )
     style_header.font.bold = True
     style_header.alignment = Alignment(horizontal="center", vertical="center")
     style_header.fill = PatternFill("solid", fgColor="76933c")
+    style_header.border = Border(left=bd, top=bd, right=bd, bottom=bd)
+
     style_content = NamedStyle(name="style_content")
     style_content.font = Font(size=10, name='Arial')
     style_content.fill = PatternFill("solid", fgColor="d8e4bc")
+    style_content.border = Border(left=bd, top=bd, right=bd, bottom=bd)
+
+    style_Cost = NamedStyle(name="style_Cost")
+    style_Cost.font = Font(size=12, name='Arial', )
+    style_Cost.font.bold = True
+    style_Cost.alignment = Alignment(horizontal="center", vertical="center")
+    style_Cost.fill = PatternFill("solid", fgColor="d8e4bc")
+    style_Cost.border = Border(left=bd, top=bd, right=bd, bottom=bd)
 
     wb.add_named_style(style_header)
     wb.add_named_style(style_content)
+    wb.add_named_style(style_Cost)
 
 
-def setContentDataFrame(arrayUnformatedData: List) -> List:
+def setContentDataFrame(arrayUnformattedData: List) -> List:
     data = []
-    for employeesItem in arrayUnformatedData:
-        formatedCoast = math.ceil(employeesItem.cost * 100)/100
+    for employeesItem in arrayUnformattedData:
+        formattedCoast = math.ceil(employeesItem.cost * 100)/100
         aux = [employeesItem.data,
                employeesItem.name,
                employeesItem.function,
                employeesItem.exams,
                employeesItem.typeExam,
-               formatedCoast]
+               formattedCoast]
+        data.append(aux)
+    return data
+
+
+def setContentDetailDataFrame(arrayUnformattedData: List) -> List:
+    data = []
+    for employeesItem in arrayUnformattedData:
+        listOfExams = []
+        listOfCost = []
+        for examCost in employeesItem.examsCost:
+            formattedCoastDetail = math.ceil(examCost[1] * 100)/100
+            listOfExams.append(examCost[0])  # name exam
+            listOfCost.append(formattedCoastDetail)  # value exam
+
+        aux = [employeesItem.data,
+               employeesItem.name,
+               employeesItem.function,
+               listOfExams,
+               employeesItem.typeExam,
+               listOfCost]
         data.append(aux)
     return data
 
@@ -190,17 +228,25 @@ def mountStyleHeaderTable(monthNumber: int, ws: Worksheet, companyName: str):
 
 def mountStyleContentTable(ws, dataCompany):
     columnsTable = ['A', 'B', 'C', 'D', 'E', 'F']
+    count_row = ws.max_row
 
     # Conteúdo da tabela
-    for i in range(0, len(dataCompany)):
+    for i in range(0, count_row-1):
         for y in columnsTable:
             ws[f'{y}{i+2}'].style = 'style_content'
-            if (y == 'A'):
-                ws[f'{y}{i+2}'].alignment = Alignment(
-                    horizontal="center", vertical="center")
+            # if (y == 'A'):
+            #     ws[f'{y}{i+2}'].alignment = Alignment(
+            #         horizontal="center", vertical="center")
+            ws[f'{y}{i+2}'].alignment = Alignment(
+                horizontal="center", vertical="center")
             if (y == 'F'):
                 number_forma = '"$"#,##0.00_);[Red]("$"#,##0.00)'
                 ws[f'{y}{i+2}'].number_format = number_forma
+                ws[f'{y}{i+2}'].alignment = Alignment(
+                    horizontal="right", vertical="center")
+            if (y == 'D'):
+                ws[f'{y}{i+2}'].alignment = Alignment(
+                    horizontal="left", vertical="center")
 
 
 def resizeTable(ws):
@@ -215,12 +261,45 @@ def resizeTable(ws):
         ws.column_dimensions[col].width = value + 4
 
 
-def calculateCoast(company) -> int:
-    coast = 0
+def calculateCost(company) -> int:
+    cost = 0
     for employee in company.employees:
-        print(employee)
-        # coast += employee[5]
-    return coast
+        cost += employee.cost
+    return cost
+
+
+def setTotalCostStyle(ws):
+    columnsTable = ['A', 'B', 'C', 'D', 'E', 'F']
+    row_count = ws.max_row
+    number_forma = '"$"#,##0.00_);[Red]("$"#,##0.00)'
+    for letter in columnsTable:
+        ws[f'{letter}{row_count}'].style = 'style_Cost'
+
+    ws[f'F{row_count}'].number_format = number_forma
+
+
+def setTotalCost(ws):
+    row_count = ws.max_row
+    ws.append(['TOTAL', '', '', '', '', f"=SUM(F2: F{row_count})"])
+    ws.merge_cells(f'A{row_count+1}:E{row_count+1}')
+    setTotalCostStyle(ws)
+
+
+def addDetailDataFrame(ws, item):
+    row_count = ws.max_row
+    lenOfExams = len(item[3])
+    exams = item[3]
+    examsCost = item[5]
+    firstRow = [item[0], item[1], item[2], exams[0], item[4], examsCost[0]]
+    ws.append(firstRow)
+    for i in range(1, lenOfExams, ):
+        ws.append(['', '', '',  exams[i], '', examsCost[i]])
+
+    columnsAppend = ['A', 'B', 'C', 'E']
+    # merge_cells A, B, C, E
+    for letter in columnsAppend:
+        ws.merge_cells(
+            f'{letter}{row_count+1}:{letter}{row_count+lenOfExams}')
 
 
 def main():
@@ -331,7 +410,6 @@ def main():
             list_of_company = []
             getExams(company_name, list_of_company, newCompany)
             namesAndExams = []
-            index = 0
 
             for employees in company_Billing.employeesBilling:
                 namesAndExams.append(
@@ -346,6 +424,7 @@ def main():
                 employee_company.exams = nameAndExam[3]
                 employee_company.typeExam = nameAndExam[5]
                 employee_company.data = dateFormatted
+                employee_company.examsCost = []
                 exams_exect = nameAndExam[1].split('/')
 
                 hasExam = False
@@ -365,6 +444,9 @@ def main():
                             if (('externo' not in exam.lower() and
                                     'externo' not in company[0].lower())):
                                 employee_company.cost += company[1]
+                                if (hasDetailed):
+                                    employee_company.examsCost.append(
+                                        (exam, company[1]))
                                 hasExam = True
                     if (not hasExam):
                         if (newCompany.missingExams == ''):
@@ -388,6 +470,12 @@ def main():
 
         locale.setlocale(locale.LC_ALL, 'pt_BR')
         monthNumber = datetime.strptime(name_month, '%B').month
+        try:
+            os.mkdir('Faturamentos')
+        except FileExistsError:
+            ...
+        except Exception as e:
+            print('Error ao criar a pasta', e)
 
         for company in companyList:
             wb: Workbook = Workbook()
@@ -395,26 +483,36 @@ def main():
             wb.remove(wb['Sheet'])
             dataCompany = []
 
-            dataCompany = setContentDataFrame(company.employees)
+            if (hasDetailed):
+                dataCompany = setContentDetailDataFrame(company.employees)
+            else:
+                dataCompany = setContentDataFrame(company.employees)
 
             ws: Worksheet = wb.create_sheet(company.name[:15])
 
             mountStyleHeaderTable(monthNumber, ws, company.name)
 
-            coast = 0
-            coast = calculateCoast(company)
-
+            # cost = 0
+            # cost = calculateCost(company)
+            # print(cost)
             for item in dataCompany:
-                ws.append(item)
+                if (hasDetailed):
+                    addDetailDataFrame(ws, item)
+                else:
+                    ws.append(item)
 
             mountStyleContentTable(ws, dataCompany)
+            setTotalCost(ws)
             resizeTable(ws)
 
             nameFile = f'{company.name} - {monthNumber} {name_month}'
-            wb.save(f'Teste/Faturamento - {nameFile} 2023.xlsx')
+            wb.save(f'Faturamentos/Faturamento - {nameFile} 2023.xlsx')
 
 # ----------------------------------------------------------------
 
 
 if __name__ == '__main__':
+    input = input("Deseja que a planilha seja detalhada ? (S/N)")
+    if (input.lower() == "s"):
+        hasDetailed = True
     main()
